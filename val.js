26167134ed05b59620891fc87176ad6503445158
DeenRamah <96067114+DeenRamah@users.noreply.github.com>
2023-02-08 07:03:34 +0300

    first commit

---
+++ val.js
@@ -0,0 +1,13 @@
+let button = document.getElementById("button")
+let boxie = document.getElementById("input-box")
+let serchbar = document.getElementById("uil uil-search")
+let itemSearch = document.getElementById("uil uil-search")
+
+const butonie =()=>{
+
+button.document.querySelector(".button");
+
+button.addEventListener("mousedown", ()=>button.classList.add(clicked));
+button.addEventListener("mouseup", ()=> button.classList.remove("clicked"));
+
+}
